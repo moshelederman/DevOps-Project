9d82b963b011ebf428e1bc93ba270b0eac7764e2
moshelederman <mos9972@gmail.com>
2025-01-07 15:09:59 +0200

    add project

---
+++ project/ScriptsActivate.ps1
@@ -0,0 +1,4 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+     2020       1    2020      20968  cons0     197609 19:51:02 /usr/bin/bash
+     2063       1    2063       7740  cons1     197609 19:51:06 /usr/bin/bash
+     2271    2020    2271      18696  cons0     197609 20:03:59 /usr/bin/PS

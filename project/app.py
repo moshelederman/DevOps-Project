10bddc96b98c8296572602213ef456945e7a5553
moshelederman <mos9972@gmail.com>
2025-01-08 14:57:34 +0200

    127.0.0.1

--- project/app.py
+++ project/app.py
@@ -56,4 +56,4 @@ def display_images():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port) # nosec
+    app.run(host="127.0.0.1", port=port) # nosec

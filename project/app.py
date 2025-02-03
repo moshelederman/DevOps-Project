b27dea2a536ffe97e7053ff20255242efb915b1d
moshelederman <mos9972@gmail.com>
2025-02-03 11:35:08 +0200

    cleaning code

--- project/app.py
+++ project/app.py
@@ -19,30 +19,23 @@ db_config = {
 @app.route('/')
 def display_images():
     try:
-        # התחברות למסד הנתונים
         cnx = mysql.connector.connect(**db_config)
         cursor = cnx.cursor()
 
-        # עדכון מונה המבקרים
         cursor.execute("UPDATE visitors SET visit_count = visit_count + 1 WHERE id = 1")
         cnx.commit()
 
-        # בדיקת המונה
         cursor.execute("SELECT visit_count FROM visitors WHERE id = 1")
         visit_count = cursor.fetchone()[0]
 
-        # שאילתת ה-SQL לשליפת תמונה אקראית
         cursor.execute("SELECT image_url FROM images ORDER BY RAND() LIMIT 1")
         result = cursor.fetchone()
 
-        # בדיקת תוצאה
         image_url = result[0] if result else None
 
-        # סגירת ההתחברות למסד הנתונים
         cursor.close()
         cnx.close()
 
-        # העברת ה-URL לתבנית להציג אותו
         return render_template('index.html', image_url=image_url, visit_count=visit_count)
 
     except mysql.connector.Error as err:
